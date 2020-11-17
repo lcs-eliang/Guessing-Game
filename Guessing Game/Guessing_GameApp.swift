54a3b9a8b0a09e8ccad4b77c58f5de56335f8686
Emily Liang <eliang@lcs.on.ca>
2020-11-17 14:43:40 -0500

    Initial Commit

---
+++ Guessing Game/Guessing_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guessing_GameApp.swift
+//  Guessing Game
+//
+//  Created by Emily Liang on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct Guessing_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
